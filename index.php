07ee76cb3f007271e52d27a68388247af911a70c
ktaeyln <ktaeyln@gmail.com>
2013-02-16 17:56:04 -0500

    Include an index.php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Require Framework Loader
+$frameworkPath = dirname(__FILE__);
+require $frameworkPath . DIRECTORY_SEPARATOR . "loader.inc.php";
+
+/*
+
+You can use the same framework install for multiple websites.
+Simple install the framework somewhere every website can access,
+and change $frameworkPath to point to its folder.
+
+*/
+
+?>
