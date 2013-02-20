7652ab763d9911b944dee2eb2e754818b37c819f
ktaeyln <ktaeyln@gmail.com>
2013-02-20 10:31:55 -0500

    Fixed typo

--- extensions/PayPal Checkout/paypal.class.php
+++ extensions/PayPal Checkout/paypal.class.php
@@ -45,7 +45,7 @@ class PayPalExpressGateway extends PaymentGateway {
 		}
 	
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://api-3t. " . self::$domain . "/nvp");
+		curl_setopt($ch, CURLOPT_URL, "https://api-3t." . self::$domain . "/nvp");
 		curl_setopt($ch, CURLOPT_VERBOSE, 1);
 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
