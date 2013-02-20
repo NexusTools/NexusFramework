dcad945329a9c7f23973e881616ffd2fd1eb7029
ktaeyln <ktaeyln@gmail.com>
2013-02-20 11:10:02 -0500

    Implmeneted gateway callbacks

---
+++ extensions/Payment Gateways/pages/virtual.exists.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$providerName = PageModule::getArgument(1);
+//throw new Exception(json_encode(PaymentGateway::getGateways()));
+$provider = PaymentGateway::getGateway($providerName);
+if($provider) 
+	$provider->handleCallback(relativepath(substr(REQUEST_URI, 27 + strlen($providerName))));
+
+return false;
+?>
