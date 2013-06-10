fe52d64c7c856b7b827f37d65d66a6a9f8eda961
ktaeyln <ktaeyln@gmail.com>
2013-06-10 12:59:42 -0400

    Separated user module from region core extension

--- extensions/Region Core/region-core.class.php
+++ extensions/Region Core/region-core.class.php
@@ -1,4 +1,19 @@
 <?php // Relies on GeoIP Extension
 class RegionCore {
+
+	public static function getRecord($addr=false) {
+		try {
+			if(!$addr)
+				$addr = ClientInfo::getRemoteAddress();
+			$record = geoip_record_by_name($addr);
+			$record['timezone'] = geoip_time_zone_by_country_and_region($record['country'], $record['region']);
+			$record['postal-code'] = $record['postal_code'];
+			
+			return $record;
+		} catch(Exception $e) {
+			return false;
+		}
+	}
+
 }
 ?>
