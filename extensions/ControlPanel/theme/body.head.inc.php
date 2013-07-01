f3ed1f26b4b638c2157cabc6d2a962e6bce39b10
ktaeyln <ktaeyln@gmail.com>
2013-07-01 10:28:57 -0400

    Added initial branding support for controlpanel

--- extensions/ControlPanel/theme/body.head.inc.php
+++ extensions/ControlPanel/theme/body.head.inc.php
@@ -1,7 +1,19 @@
 <framework:widgets>
 		<whiteout style="display: none;"><spinner></spinner><popup></popup></whiteout>
-		<toolbar style="position: relative; z-index: 100" align="left">
-			<widget><? echo BASE_URL; ?></widget>
+		<toolbar style="position: relative; z-index: 300" align="left">
+			<?
+			$logo = fullpath("logo.png");
+			$domain = StringFormat::displayForID(DOMAIN_SL);
+			if(file_exists($logo)) {
+				echo "<img style='float: left; display: inline-block; margin: 1px 5px; padding: 0px;' src='";
+				echo Framework::getReferenceURI($logo);
+				echo "' alt='";
+				echo $domain;
+				echo "' height='30px' />";
+			}
+			?><widget><? echo $domain; ?> Control Panel</widget>
+			<widget>1 User(s) Online</widget>
+			<widget>0 Notifications</widget>
 			<widget class="last">Logged in as <? echo User::getFullName(); ?>
 				<menu>
 					<a href="control://Users/Profile">Edit My Profile</a>
@@ -14,5 +26,4 @@ if(PageModule::countArguments() === 3)
     ControlPanel::dumpNavBar(PageModule::getArgument(1), PageModule::getArgument(2));
 else
     ControlPanel::dumpNavBar();
-?>
-		<uploads></uploads></navbar>
+?><uploads></uploads></navbar>
