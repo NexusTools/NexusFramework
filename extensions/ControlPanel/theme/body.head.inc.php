80ef1ad2d5418f5aa16cd559a366bfc09042bc93
ktaeyln <ktaeyln@gmail.com>
2013-07-01 10:31:29 -0400

    Added banner branding support

--- extensions/ControlPanel/theme/body.head.inc.php
+++ extensions/ControlPanel/theme/body.head.inc.php
@@ -2,16 +2,24 @@
 		<whiteout style="display: none;"><spinner></spinner><popup></popup></whiteout>
 		<toolbar style="position: relative; z-index: 300" align="left">
 			<?
-			$logo = fullpath("logo.png");
-			$domain = StringFormat::displayForID(DOMAIN_SL);
-			if(file_exists($logo)) {
-				echo "<img style='float: left; display: inline-block; margin: 1px 5px; padding: 0px;' src='";
+			$banner = fullpath("cp-banner.png");
+			if(file_exists($banner)) {
+				echo "<img style='float: left; display: inline-block; margin: 0px 2px; padding: 0px;' src='";
 				echo Framework::getReferenceURI($logo);
 				echo "' alt='";
 				echo $domain;
-				echo "' height='30px' />";
-			}
-			?><widget><? echo $domain; ?> Control Panel</widget>
+				echo "' height='32px' />";
+			} else {
+				$logo = fullpath("logo.png");
+				$domain = StringFormat::displayForID(DOMAIN_SL);
+				if(file_exists($logo)) {
+					echo "<img style='float: left; display: inline-block; margin: 1px 5px; padding: 0px;' src='";
+					echo Framework::getReferenceURI($logo);
+					echo "' alt='";
+					echo $domain;
+					echo "' height='30px' />";
+				}
+			?><widget><? echo $domain; ?> Control Panel</widget><? } ?>
 			<widget>1 User(s) Online</widget>
 			<widget>0 Notifications</widget>
 			<widget class="last">Logged in as <? echo User::getFullName(); ?>
