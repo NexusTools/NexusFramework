6073a95f7f7cd7fe35c9378e6b29c1f518506372
ktaeyln <ktaeyln@gmail.com>
2013-07-01 10:44:39 -0400

    Added support for control panel css themes

--- extensions/ControlPanel/pages/root.head.inc.php
+++ extensions/ControlPanel/pages/root.head.inc.php
@@ -9,10 +9,13 @@ $base = dirname(dirname(__FILE__)) . DIRSEP;
 $owdir = getcwd();
 chdir($base);
 Template::addScript($base . "cp-script.js");
-Template::addStyle(FRAMEWORK_PATH . "resources/stylesheets/widgets.css");
-Template::addStyle($base . "cp-style.css");
+Template::addStyles(Array(FRAMEWORK_PATH . "resources/stylesheets/widgets.css", $base . "cp-style.css"));
 if(array_key_exists("popup", $_GET))
 	Template::addStyle($base . "cp-popup.css");
+
+$externStyle = fullpath("cp-theme.css");
+if(file_exists($externStyle))
+	Template::addStyle($externStyle);
 
 requireAddon("eventable-object");
 requireAddon("unfinished-work");
