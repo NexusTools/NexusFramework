fe2a9e055b2c1a0837aa149cdc7cba004f16fdd9
Katelyn <ktaeyln@gmail.com>
2013-09-01 14:29:33 -0400

    Implemented theme parenting

--- modules/page-system/Theme.class.php
+++ modules/page-system/Theme.class.php
@@ -2,9 +2,22 @@
 class Theme extends CachedFile {
 
     private static $activePath;
+    private $parent = false;
     
-    public static function initActivePath(){
-    	self::$activePath = INDEX_PATH . "themes" . DIRSEP . THEME;
+    protected static function findThemePath($name) {
+    	if(file_exists($path = INDEX_PATH . "themes" . DIRSEP . $name))
+    		return $path;
+    		
+    	if(file_exists($path = FRAMEWORK_RES_PATH . "themes" . DIRSEP . $name))
+    		return $path;
+    		
+    	return false;
+    }
+    
+    public static function initActivePath() {
+    	if(!defined("THEME"))
+    		define("THEME", "Blank");
+    	self::$activePath = self::findThemePath(THEME);
     }
     
     public static function getPath(){
@@ -23,20 +36,26 @@ class Theme extends CachedFile {
 		return "theme";
 	}
 	
+	public function findFile() {
+	}
+	
 	protected function update(){
 		$data = Array();
+		
+		if(file_exists($this->getFilepath() . "info.json")) {
+			$data['metadata'] = LocalFile::getContentFor($this->getFilepath() . "info.json");
+			if(!$data['metadata'])
+				throw new Exception("Theme info.json file corrupt");
+		}
+		
 		if(file_exists($this->getFilepath() . "head.inc.php"))
 			$data['hs'] = "head.inc.php";
 		
 		if(file_exists($this->getFilepath() . "body.head.inc.php"))
 			$data['bhs'] = "body.head.inc.php";
-		else
-			throw new IOException($this->getFilepath() . "body.head.inc.php", IOException::NotFound, "Missing Theme Body Header");
 		
 		if(file_exists($this->getFilepath() . "body.foot.inc.php"))
 			$data['bfs'] = "body.foot.inc.php";
-		else
-			throw new IOException($this->getFilepath() . "body.foot.inc.php", IOException::NotFound, "Missing Theme Body Footer");
 			
 		$data['classes'] = Array();
 		if(file_exists($this->getFilepath() . "classes")){
@@ -70,6 +89,17 @@ class Theme extends CachedFile {
 	}
 	
 	public function initialize(){
+		if($this->hasKey("error"))
+			throw new Exception($this->getValue('error'));
+			
+		if($this->hasKey("metadata")) {
+			$metadata = $this->getValue("metadata");
+			if(array_key_exists("parent", $metadata)) {
+				$this->parent = new Theme(self::findThemePath($metadata["parent"]));
+				$this->parent->initialize();
+			}
+		}
+	
 		if($this->hasKey('hs')) 
 			require_chdir($this->getValue('hs'), $this->getFilepath());
 		
@@ -79,11 +109,21 @@ class Theme extends CachedFile {
 	}
 	
 	public function runHeader(){
-		require_chdir($this->getValue('bhs'), $this->getFilepath());
+		if($this->hasKey("bhs"))
+			require_chdir($this->getValue('bhs'), $this->getFilepath());
+		else if($this->parent)
+			$this->parent->runHeader();
+		else
+			throw new Exception("Theme has no header file.");
 	}
 	
 	public function runFooter(){
-		require_chdir($this->getValue('bfs'), $this->getFilepath());
+		if($this->hasKey("bfs"))
+			require_chdir($this->getValue('bfs'), $this->getFilepath());
+		else if($this->parent)
+			$this->parent->runFooter();
+		else
+			throw new Exception("Theme has no footer file.");
 	}
 
 } Theme::initActivePath();
