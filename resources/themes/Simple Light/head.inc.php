d7591778e6164959d41b16d35e3cf79b7d920c61
Katelyn <ktaeyln@gmail.com>
2013-09-01 12:09:19 -0400

    Added a light theme, changed order of Simple and Dark

---
+++ resources/themes/Simple Light/head.inc.php
@@ -0,0 +1,5 @@
+<?php
+Template::addStyle(FRAMEWORK_RES_PATH . "stylesheets" . DIRSEP . "widgets.css");
+Template::setFavicon(FRAMEWORK_RES_PATH . "icons" . DIRSEP . "favicon.ico");
+Template::addStyle("style.css");
+?>
