c55b99cfd5ff1b9ff608c062196ea62977771e06
Katelyn <ktaeyln@gmail.com>
2013-09-02 14:57:43 -0400

    Use table-cell style for columns by default

--- resources/themes/Simple Blog/head.inc.php
+++ resources/themes/Simple Blog/head.inc.php
@@ -1,4 +1,4 @@
 <?php
-Template::addStyle(FRAMEWORK_RES_PATH . "stylesheets" . DIRSEP . "widgets.css");
-Template::addStyle("style.css");
+Template::addExternalStyle("http://fonts.googleapis.com/css?family=Open+Sans");
+Template::addStyles(Array("styles/layout.css", "styles/page.css"));
 ?>
---
+++ resources/themes/Simple Blog/tmpl.head.inc.php
@@ -0,0 +1,1 @@
+<a class="logo" href="<? echo BASE_URI; ?>"><img src="<? echo Framework::getReferenceURI("images/banner.png"); ?>" /></a><container>
