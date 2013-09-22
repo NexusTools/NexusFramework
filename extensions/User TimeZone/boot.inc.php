525f077cd930d49f046bb8deedc4a0c4b36f1741
Katelyn <ktaeyln@gmail.com>
2013-09-22 13:08:58 -0400

    Added timezone extension

---
+++ extensions/User TimeZone/boot.inc.php
@@ -0,0 +1,10 @@
+<?php
+User::registerExtension("TimeZoneUser");
+
+function __tzUser_userChangeTriggerCallback($module, $event, $arguments) {
+	if($module == "User" && $event == "Changed")
+		date_default_timezone_set(User::getTimeZone());
+}
+
+Triggers::watchModule("User", "__tzUser_userChangeTriggerCallback");
+?>
---
+++ extensions/User TimeZone/tzuser.class.php
@@ -0,0 +1,27 @@
+<?php
+class TimeZoneUser {
+    
+    private $uid;
+    private static $database = false;
+    private static $defaultTZ = false;
+    
+    public static function getDatabase() {
+        return self::$database === false ? (self::$database = Database::getInstance()) : self::$database;
+    }
+
+    public function __construct($user) {
+        $this->uid = $user->getID();
+    }
+    
+    public function getTimeZone(){
+    	$db = self::getDatabase();
+    	$tz = $db->selectField("timezone", array("user" => $this->uid), "timezone");
+    	if(!self::$defaultTZ)
+    		self::$defaultTZ = date_default_timezone_get();
+    	if(!$tz)
+    		$tz = self::$defaultTZ;
+    	return $tz
+    }
+
+}
+?>
