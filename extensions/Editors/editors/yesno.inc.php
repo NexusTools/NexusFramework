e886d2dec15a90b24735858e91ad5b43dc72cbc6
Katelyn <ktaeyln@gmail.com>
2013-09-30 10:53:37 -0400

    Added padding to yesno editor

--- extensions/Editors/editors/yesno.inc.php
+++ extensions/Editors/editors/yesno.inc.php
@@ -1,14 +1,15 @@
 <?php
 switch($mode){
 	case EditCore::RENDER:
-		?><input type="radio" value="1" name="<? echo $name; ?>" id="__cp_<? echo $name; ?>_inNavbarYes"<?
+		?><widget class="component" style="line-height: 26px; margin-bottom: 6px">
+<input type="radio" value="1" name="<? echo $name; ?>" id="__cp_<? echo $name; ?>_inNavbarYes"<?
 if($value || $value == "Yes")
 	echo " checked";
 ?> /><label for="__cp_<? echo $name; ?>_inNavbarYes">Yes</label> 
 <input type="radio" value="0" name="<? echo $name; ?>" id="__cp_<? echo $name; ?>_inNavbarNo"<?
 	if(!$value)
 		echo " checked";
-?> /><label for="__cp_<? echo $name; ?>_inNavbarNo">No</label><?
+?> /><label for="__cp_<? echo $name; ?>_inNavbarNo">No</label></widget><?
 	
 	case EditCore::VALIDATE:
 		return true;
