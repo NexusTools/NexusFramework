cd580a26e155c43ff486fd3191c9c60c540f103d
Katelyn <ktaeyln@gmail.com>
2013-10-08 21:27:32 -0400

    Implemented StringFormat.displayForID

--- resources/javascript/framework/02_stringformat.js
+++ resources/javascript/framework/02_stringformat.js
@@ -1,7 +1,17 @@
 Framework.registerModule("StringFormat", {
 	
-		idForDisplay: function(id){
-			return id;
+		idForDisplay: function(display){
+			return display;
+		},
+		
+		displayForID: function(id){
+			console.log("displayForID");
+		
+			return id.replace(/[^\w\d]+/g, " ")
+					.replace(/(\w)(\w+)/g, function(match, p1, p2) {
+				console.log("match", arguments);
+				return p1.toUpperCase() + p2.toLowerCase();
+			});
 		}
 	
 	
